edd8c0155ed5685d254d27e0a207d656d39cf977
An Nguyen <cuvitdet15@gmail.com>
2022-12-30 15:12:39 +0700

    first commit

---
+++ api/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="success.php" method="post">
+        Link post: <input type="text" name="link"><br>
+        Số lượng tương tác cần: <input type="number" name="quantity" min="50" max="50"><br>
+        Dịch vụ:
+        <select name="services" id="services">
+            <option value="199">Like</option>
+            <option value="200">Love</option>
+        </select>
+        <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
